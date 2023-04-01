55198bbdee3a5d7f720ee1fb0a802151ad0bda0c
Tomasz Steć <tomeczekstecc@gmail.com>
2023-04-02 00:02:40 +0200

    update projects

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import Image from 'next/image'
-import {Link} from "react-scroll/modules";
 
 const techs = [
     {name: 'React'},
--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from "next/image";
 import Link from "next/link";
-import {BsArrowUpRightSquare, BsGithub} from "react-icons/bs";
+import {BsArrowUpRightSquare} from "react-icons/bs";
 import {SiGitlab} from "react-icons/si";
 import SlideUp from "@/components/SlideUp";
 
