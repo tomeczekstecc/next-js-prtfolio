fbf62f471ae8a4ecf60a0eb7e5f71c45344c0e63
Tomasz Steć <tomeczekstecc@gmail.com>
2023-04-02 10:57:07 +0200

    footer addjust

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,56 +1,68 @@
-import React from "react"
+import React from 'react'
 import {
     AiOutlineGithub,
     AiOutlineTwitter,
     AiOutlineLinkedin,
     AiOutlineYoutube,
-} from "react-icons/ai"
+    AiFillGitlab,
+} from 'react-icons/ai'
 
 const Footer = () => {
     return (
-        <footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
-            <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
-            <div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
-                <div
-                    className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
-                    © 2023 Tomasz Steć<a title={'link do sekcji głównej'} href="/" className="hover:underline"></a>
+        <footer className='mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl '>
+            <hr className='w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0'></hr>
+            <div className='mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between'>
+                <div className='flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100'>
+                    © 2023 Tomasz Steć
+                    <a
+                        title={'link do sekcji głównej'}
+                        href='/'
+                        className='hover:underline'
+                    ></a>
                 </div>
-                <div className="flex flex-row items-center justify-center space-x-2 mb-1">
-                    <a title={'link do social media'} href="https://github.com/hqasmei" rel="noreferrer"
-                       target="_blank">
+                <div className='flex flex-row items-center justify-center space-x-2 mb-1'>
+                    <a
+                        title={'link do social media'}
+                        href='https://github.com/tomeczekstecc'
+                        rel='noreferrer'
+                        target='_blank'
+                    >
                         <AiOutlineGithub
-                            className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+                            className='hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100'
                             size={30}
                         />
                     </a>
-                    <a title={'link do social media'}
-                       href="https://twitter.com/hqasmei"
-                       rel="noreferrer"
-                       target="_blank"
+                    <a
+                        title={'link do social media'}
+                        href='https://gitlab.com/tomeczekstecc'
+                        rel='noreferrer'
+                        target='_blank'
+                    >
+                        <AiFillGitlab
+                            className='hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100'
+                            size={30}
+                        />
+                    </a>
+                    <a
+                        title={'link do social media'}
+                        href='https://twitter.com/tomeczekstecc'
+                        rel='noreferrer'
+                        target='_blank'
                     >
                         <AiOutlineTwitter
-                            className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+                            className='hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100'
                             size={30}
                         />
                     </a>
 
-                    <a title={'link do social media'}
-                       href="https://www.linkedin.com/in/hosnaqasmei/"
-                       rel="noreferrer"
-                       target="_blank"
+                    <a
+                        title={'link do social media'}
+                        href='https://www.linkedin.com/in/tomasz-ste%C4%87-748726180/'
+                        rel='noreferrer'
+                        target='_blank'
                     >
                         <AiOutlineLinkedin
-                            className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
-                            size={30}
-                        />
-                    </a>
-                    <a title={'link do social media'}
-                       href="https://www.youtube.com/channel/UCQBMkSDgbxDb8usMeXmOZyA"
-                       rel="noreferrer"
-                       target="_blank"
-                    >
-                        <AiOutlineYoutube
-                            className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+                            className='hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100'
                             size={30}
                         />
                     </a>
