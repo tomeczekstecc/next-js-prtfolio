d21d7f047ad815db1a4613e1ec58b01944bccfd6
Tomasz Steć <tomeczekstecc@gmail.com>
2024-06-27 15:08:02 +0200

    revalidate requests

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [{
-            protocol: 'https',
-            hostname: 'portfolio-dev-strapi.hlts.duckdns.org',
-        }],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'portfolio-dev-strapi.hlts.duckdns.org',
+            },
+            {
+                protocol: 'https',
+                hostname: 'portfolio-strapi-dev.tslab.live'
+            }
+
+        ],
     }
 }
 
