9967abd9dd3bf2c462edec20ea56ecd4218b8047
Tomasz Steć <tomeczekstecc@gmail.com>
2024-06-27 11:47:31 +0200

    revalidate requests

--- app/actions/getAllProjects.ts
+++ app/actions/getAllProjects.ts
@@ -2,7 +2,11 @@
 import * as process from "node:process";
 
 const getAllProjects = async () => {
-    const data = await fetch(process.env.NEXT_PUBLIC_STRAPI_URL + '/api/projects?populate=*')
+    const data = await fetch(process.env.NEXT_PUBLIC_STRAPI_URL + '/api/projects?populate=*', {
+        next: {
+            revalidate: 60
+        }
+    })
     const projects = await data.json()
     return projects.data
 }
