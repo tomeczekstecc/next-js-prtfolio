e4786203414ca27dfcf6f4a6fb78da86163efda7
Tomasz Steć <tomeczekstecc@gmail.com>
2024-06-27 11:47:25 +0200

    revalidate requests

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -7,69 +7,6 @@ import SlideUp from "@/components/SlideUp";
 import getAllProjects from "@/app/actions/getAllProjects";
 import * as process from "node:process";
 
-const projects = [
-    {
-        name: "Portal ze sklepem i blogiem",
-        description:
-            "Portal umożliwiający przeglądanie, usuwanie, edytowanie blogów, komentowania ich, przeglądanie, usuwanie, edytowanie filmów oraz pełny sklep z obsługą koszyka z funkcją dokonywania płatności za pomocą karty, gpay, blik oraz przelewów bankowych. Portal obsługuje w pełni funkcjonalny system obsługi autoryzacji i autentykacji.  ",
-        image: "/project_6.png",
-        repo: "https://gitlab.com/tomeczekstecc/martyna-tv",
-        link: "https://martyna-tv.vercel.app/store/",
-        icon: <SiGitlab size={30}/>,
-        techs: ['React', 'TypeScript', 'NextAuth.js', 'NextJS', 'Stripe', 'ShadCNUI', 'TailwindCSS', 'Node.js', 'Prisma', 'Redux/Redux Toolkit', 'tRPC']
-    },
-    {
-        name: "Ocena wniosków stypendialnych",
-        description:
-            "Aplikacja służąca do zarządzania wnioskami stypendialnymi, dokonywania oceny wniosków, zarządzania ocenami, przydzielania oceniających oraz udostępniania materiałów pomocniczych. Obsługuje cały proces po złożeniu wniosku przez ucznia lub opiekuna do jego oceny przez oceniającego, po zatwierdzenie przez menadżera.",
-        image: "/project_1.png",
-        repo: "https://gitlab.com/tomeczekstecc/ocena-web-chakra-ui",
-        link: "https://stypendia-ocena-dev.tomaszstec.me/",
-        icon: <SiGitlab size={30}/>,
-        techs: ['React', 'TypeScript', 'Chakra UI', 'Node.js', 'Express.js', 'GraphQL', 'MySQL', 'TypeORM', 'Docker', 'GitLab CI/CD', 'Keycloak']
-    },
-
-    {
-        name: "Data overview",
-        description: "Aplikacja służąca do przeglądu danych statystycznych, np. sprzedażowych, oferująca różne ciekawe wizualizacje danych, w różnych konfigurowalnych przez użytkownika układach. Wersja poglądowa, ponieważ hostowana jest na darmowyej platwormie, pierwsze uruchomienie może zająć ok minuty.",
-        image: "/project_2.png",
-        repo: "https://gitlab.com/tomeczekstecc/data-overview",
-        link: "https://fullstack-dashboard-client-2g2w.onrender.com",
-        icon: <SiGitlab size={30}/>,
-        techs: ['React', 'Vite', 'Material UI', 'Node.js', 'Express.js', 'Redux', 'REST Api', 'Docker', 'Mongo DB']
-
-    },
-    {
-        name: "Ecommerce Shop",
-        description:
-            "Testowy sklep z obsługą płatności przy użyciu Stripe oraz cmsa Strapi. Zapewnia przegląd produktów np. wg kategorii, obsługuje koszyk, formularz zamówienia oraz płatności.",
-        image: "/project_3.png",
-        repo: "hhttps://gitlab.com/tomeczekstecc/ecommerce-shop-strapi",
-        link: "/",
-        icon: <SiGitlab size={30}/>,
-        techs: ['React', 'Strapi', 'Stripe', 'Material UI', 'Redux', 'Formik', 'Docker', 'GitLab CI/CD']
-    },
-    {
-        name: "Wyszukiwarka filmów",
-        description:
-            "Wyszukiwarke filmów z wykorzystaniem API The Movie Database. Zapewnia przegląd filmów, ich szczegółowe informacje",
-        image: "/project_5.png",
-        repo: "https://gitlab.com/tomeczekstecc/moviesdb",
-        link: "https://moviesdb.tomaszstec.me",
-        icon: <SiGitlab size={30}/>,
-        techs: ['React', 'NextJS', 'React-Query', 'REST API', 'Tailwind CSS', 'Vercel']
-    },
-    {
-        name: "Markdown Blog",
-        description:
-            "Blog utworzony przy pomocy NextJS, Gray-Matter i Remark. Zapewnia możliwość tworzenia postów z plików markdown, które są automatycznie przetwarzane na wpisy postów na stronie. Wykorzystano strategie Static Generation, dzięki której strona uzyskała maksymalną ilosć punktów w Lighthouse.",
-        image: "/project_4.png",
-        repo: "https://gitlab.com/tomeczekstecc/nextjs-markdown-blog",
-        link: "https://blog.tomaszstec.me",
-        icon: <SiGitlab size={30}/>,
-        techs: ['React', 'NextJS', 'Gray-Matter', 'Markdown', 'Remark', 'Tailwind CSS', 'Vercel']
-    },
-]
 
 type Image = {
     data: {
