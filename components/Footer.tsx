433eba0355609157cb19b2a0fd826dc16c4e6e0d
Tomasz Steć <tomeczekstecc@gmail.com>
2024-06-30 11:47:13 +0200

    footer update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
             <div className='mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between'>
                 <div
                     className='flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100'>
-                    © 2023 Tomasz Steć
+                    © 2023-2024 Tomasz Steć
                     <a
                         title={'link do sekcji głównej'}
                         href='/'
